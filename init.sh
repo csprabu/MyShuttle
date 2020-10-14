0fc219e3abd715e8a1eedd4b85aada8b98d8fd59
Prabushankar Chinnasamy <prabushankar.chinna@gmail.com>
2020-10-14 16:42:43 +0530

    + :)

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo ":)"
 set -x \
  && curl -fSL "https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m`" -o /usr/local/bin/docker-compose \
  && chmod +x /usr/local/bin/docker-compose \
